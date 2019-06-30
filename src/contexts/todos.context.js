9639bd02768d701953ebe8703e2162a19c4e1dce
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-30 18:02:00 -0400

    reducer with LocalStorage

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
 import todoReducer from "../reducers/todo.reducer";
+import { useLocalStorageReducer } from "../hooks/useLocalStorageReducer";
 
 const defaultTodos = [
   {
@@ -22,7 +23,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
 
   return (
     <TodosContext.Provider value={todos}>
---
+++ src/hooks/useLocalStorageReducer.js
@@ -0,0 +1,19 @@
+import { useEffect, useReducer } from "react";
+
+export function useLocalStorageReducer(key, defaultVal, reducer) {
+  const [state, dispatch] = useReducer(reducer, defaultVal, () => {
+    let val;
+    try {
+      val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    } catch (e) {
+      val = defaultVal;
+    }
+    return val;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
+
+  return [state, dispatch];
+}
